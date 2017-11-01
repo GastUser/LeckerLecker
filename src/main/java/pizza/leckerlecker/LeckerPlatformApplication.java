b8985eba510db24c0ae35b78b2375b9c2d3f9fbc
Chilliger <Chilliger@GIGABYTE-PC.fritz.box>
2017-11-01 19:11:44 +0100

    Kurs

--- src/main/java/pizza/leckerlecker/LeckerPlatformApplication.java
+++ src/main/java/pizza/leckerlecker/LeckerPlatformApplication.java
@@ -1,4 +1,4 @@
-package pizza.leckerlecker.leckerplatform;
+package pizza.leckerlecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
