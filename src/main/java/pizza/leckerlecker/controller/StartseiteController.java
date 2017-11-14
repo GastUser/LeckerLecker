b371295b120b553d6f65b9b24a1bd9c659f282f2
Marco Täschner <clubfahrer@me.com>
2017-11-15 00:26:42 +0100

    initial layout

--- src/main/java/pizza/leckerlecker/controller/StartseiteController.java
+++ src/main/java/pizza/leckerlecker/controller/StartseiteController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pizza.leckerlecker.controller;
 
 import org.springframework.stereotype.Controller;
@@ -10,18 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
- * @author Chilliger
+ * @author marco
  */
 @Controller
 public class StartseiteController {
- 
+    
     @GetMapping("/")
-    
-    public String startseite(){
-    return "startseite";
-    
-    
-    
+    public String startseite() {
+        return "index";
     }
-            
+    
+    @GetMapping("/listing")
+    public String listing() {
+        
+        return "listing";
+    }
+    
 }
