c70410161aedd61700f448951752a2fa5aab0d5e
Chilliger <Chilliger@GIGABYTE-PC.fritz.box>
2017-12-06 19:09:40 +0100

    Merge origin/master
    Conflicts:
    	src/main/resources/templates/listing.html

--- src/main/java/pizza/leckerlecker/controller/StartseiteController.java
+++ src/main/java/pizza/leckerlecker/controller/StartseiteController.java
@@ -1,11 +1,15 @@
 package pizza.leckerlecker.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pizza.leckerlecker.entity.Lieferant;
+import pizza.leckerlecker.entity.repository.LieferantRepository;
 
 /**
  *
@@ -13,20 +17,32 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class StartseiteController {
-    
+
+    @Autowired
+    LieferantRepository lieferantRepository;
+
     @GetMapping("/")
     public String startseite() {
+
+        List<Lieferant> alleLieferanten = lieferantRepository.findAll();
+
+        if (null == alleLieferanten || alleLieferanten.size() < 1) {
+            Lieferant neuerLieferant1 = new Lieferant("Hallo Pizza", "0800 123", "http://www.pizza.de", "http://www.pizza.de/menue");
+            lieferantRepository.save(neuerLieferant1);
+
+        }
+        System.out.println("Anzahl :" + lieferantRepository.count() );
         return "index";
     }
-    
+
     @GetMapping("/listing")
     public String listing(@RequestParam(value = "plz_ort", required = false) String location, Model rucksack) {
-        
+
         System.out.println("Eingabe: " + location);
-        
+
         rucksack.addAttribute("sucheingabe", location);
-        
+
         return "listing";
     }
-    
+
 }
