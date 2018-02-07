12f684df607ce7ecc35476a5609ad7166ecf9c1c
Chilliger <Chilliger@GIGABYTE-PC.fritz.box>
2018-02-07 19:09:45 +0100

    Merge origin/master
    Conflicts:
    	src/main/resources/templates/listing.html

--- src/main/java/pizza/leckerlecker/controller/AdminLieferant.java
+++ src/main/java/pizza/leckerlecker/controller/AdminLieferant.java
@@ -1,16 +1,11 @@
 package pizza.leckerlecker.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import static org.hibernate.validator.internal.util.CollectionHelper.newArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import pizza.leckerlecker.entity.Lieferant;
 import pizza.leckerlecker.entity.repository.LieferantRepository;
 
 /**
@@ -20,14 +15,19 @@ import pizza.leckerlecker.entity.repository.LieferantRepository;
 @Controller
 public class AdminLieferant {
 
+    private final Logger log = LoggerFactory.getLogger(AdminLieferant.class);
     @Autowired
     LieferantRepository lieferantRepository;
 
-    @GetMapping("/admin/lieferanten")
+    @GetMapping("/admin-lieferanten")
     public String AdminLieferant() {
 
-        return "index";
+        return "admin.lieferanten";
+    }
+
+    @PostMapping("/admin-lieferanten")
+    public String speichereLieferant() {
+        log.info("Lieferanten speichern!");
+        return "admin.lieferanten";
     }
 }
-
-   
