372580bd735dcae6d169f4125323cd8aa4032d0d
Chilliger <Chilliger@GIGABYTE-PC.fritz.box>
2018-03-21 19:58:25 +0100

    Merge origin/master
    Conflicts:
    	src/main/resources/templates/listing.html

--- src/main/java/pizza/leckerlecker/controller/AdminLieferant.java
+++ src/main/java/pizza/leckerlecker/controller/AdminLieferant.java
@@ -1,12 +1,15 @@
 package pizza.leckerlecker.controller;
 
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import pizza.leckerlecker.entity.Lieferant;
 import pizza.leckerlecker.entity.repository.LieferantRepository;
 
 /**
@@ -21,17 +24,32 @@ public class AdminLieferant {
     LieferantRepository lieferantRepository;
 
     @GetMapping("/admin-lieferanten")
-    public String AdminLieferant() {
+    public String AdminLieferant(Model model) {
+        Lieferant lieferant = new Lieferant();
+        lieferant.setPlz("04275");
+        lieferant.setOrt("lieferant");
+
+        model.addAttribute("lieferant", lieferant);
 
         return "admin.lieferanten";
     }
 
     @PostMapping("/admin-lieferanten")
-    public String speichereLieferant(
-            @RequestParam(value = "lieferantenname", required = false) String lieferantenName,
-            @RequestParam(value = "telefon", required = false) String telefon
-    ) {
-log.info("Lieferanten speichern: " + lieferantenName + "Phone: " + telefon);
+    public String speichereLieferant(@Valid Lieferant lieferant, BindingResult result,Model model) {
+        log.info("Lieferanten speichern: " + lieferant.getName() + "Phone :" + lieferant.getTelefon());
+        
+        if(result.hasErrors()) {
+            log.info("Fehler vorhanden");
+
+            int errorCount = result.getErrorCount();
+            String error = result.getAllErrors().get(0).toString();
+            log.info("Anzahl Fehler: " + errorCount);
+            log.info("Fehler: " + error);
+
+        }
+
         return "admin.lieferanten";
+
     }
+
 }
--- src/main/java/pizza/leckerlecker/entity/Lieferant.java
+++ src/main/java/pizza/leckerlecker/entity/Lieferant.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  *
  * @author marco
@@ -13,7 +16,12 @@ public class Lieferant {
 
     @Id
     @GeneratedValue
+
+    
+
     private Long id;
+    @NotNull
+    @Size(min = 2, max = 80)
     private String name;
     private String telefon;
     private String webseite;
