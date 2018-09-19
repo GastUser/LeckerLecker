692461a0f1cd01dfaa0c6f73a1b27fd96b4e3b05
Chilliger <Chilliger@GIGABYTE-PC.fritz.box>
2018-09-19 19:01:45 +0200

    Merge origin/master
    Conflicts:
    	src/main/resources/templates/listing.html

---
+++ src/main/java/pizza/leckerlecker/controller/SuchdateiController.java
@@ -0,0 +1,38 @@
+package pizza.leckerlecker.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import pizza.leckerlecker.entity.Lieferant;
+import pizza.leckerlecker.entity.repository.LieferantRepository;
+
+@RestController
+public class SuchdateiController {
+
+    private final Logger log = LoggerFactory.getLogger(SuchdateiController.class);
+
+    @Autowired
+    LieferantRepository lieferantRepository;
+
+    @RequestMapping(value = "/searchdata", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    List<String> searchdata() {
+        List<String> ret = new ArrayList<>();
+        List<Lieferant> findAll = lieferantRepository.findAll();
+
+        findAll.forEach((lieferant) -> {
+            String plzOrtKombination = lieferant.getPlz() + " " + lieferant.getOrt();
+            if (!ret.contains(plzOrtKombination)) {
+                ret.add(plzOrtKombination);
+            }
+        });
+        return ret;
+    }
+}
