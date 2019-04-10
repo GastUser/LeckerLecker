6a05f7f4e3b720a121777569c6bc71a933f74d84
Marco T <marco.taeschner@me.com>
2019-04-10 16:30:25 +0200

    Add default test profile

--- src/test/java/pizza/leckerlecker/leckerplatform/LeckerPlatformApplicationTests.java
+++ src/test/java/pizza/leckerlecker/leckerplatform/LeckerPlatformApplicationTests.java
@@ -10,6 +10,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("default")
 public class LeckerPlatformApplicationTests {
 
     @Autowired
